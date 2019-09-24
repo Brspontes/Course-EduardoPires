c6931573a9f80601ab0ab5cb509f4c8f7a9b63e8
Brian Pontes <brian.robert16@hotmail.com>
2019-09-23 22:38:41 -0300

    Setup Controller and Startup

---
+++ REST-ASPNETCORE-WEBAPI/Project/ProjectWebAPI/Src/Dev.IO.API/Controllers/FornecedorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Dev.IO.API.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.IO.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public abstract class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository fornecedorRepository;
+        private readonly IMapper mapper;
+
+        protected FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            this.fornecedorRepository = fornecedorRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = mapper.Map<IEnumerable<FornecedorViewModel>>
+                (await fornecedorRepository.ObterTodos());
+            return Ok(fornecedor);
+        }
+    }
+}
---
+++ REST-ASPNETCORE-WEBAPI/Project/ProjectWebAPI/Src/Dev.IO.API/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dev.IO.API.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.IO.API.Controllers
+{
+    
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        
+    }
+}
